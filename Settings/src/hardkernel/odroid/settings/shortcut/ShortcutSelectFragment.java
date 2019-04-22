b67503e1ee2c057f2c41b1b29c0eec05b360e335
Luke Go <luke.go@hardkernel.com>
2019-04-22 15:22:53 +0900

    ODROIDSettings: Shortcut: Add initial shortcut mapping feature.
    Change-Id: I1edc138a952f71ce0a17054b318bed41cf652a8b
    Signed-off-by: Luke go <sangch.go@gmail.com>

---
+++ Settings/src/hardkernel/odroid/settings/shortcut/ShortcutSelectFragment.java
@@ -0,0 +1,84 @@
+package hardkernel.odroid.settings.shortcut;
+
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import android.view.KeyEvent;
+
+import hardkernel.odroid.settings.LeanbackAddBackPreferenceFragment;
+import hardkernel.odroid.settings.R;
+import hardkernel.odroid.settings.RadioPreference;
+
+import java.util.ArrayList;
+
+public class ShortcutSelectFragment extends LeanbackAddBackPreferenceFragment {
+    private static final String TAG = "ShortcutSelectFragment";
+    private static final String NO_SHORTCUT = "No shortcut";
+
+    private static PackageManager pm;
+    private static int keycode = KeyEvent.KEYCODE_F7;
+
+    public static ShortcutSelectFragment newInstance() {
+        return new ShortcutSelectFragment();
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        updatePreferenceFragment();
+        pm = getContext().getPackageManager();
+    }
+
+    private static ArrayList<String> appPackageList;
+    private void updatePreferenceFragment() {
+        final Context themedContext = getPreferenceManager().getContext();
+        final PreferenceScreen screen = getPreferenceManager().createPreferenceScreen(themedContext);
+        screen.setTitle(R.string.app_shortcut);
+        setPreferenceScreen(screen);
+
+        appPackageList = ShortcutManager.getAvailableAppList(getContext());
+        final ArrayList<String> appTitles = new ArrayList<>();
+
+        appTitles.add(NO_SHORTCUT);
+
+        for(String appPackage: appPackageList) {
+            appTitles.add(appPackage);
+        }
+
+        for(String appTitle: appTitles) {
+            RadioPreference radio = new RadioPreference(themedContext);
+            if (appTitle.equals(NO_SHORTCUT)) {
+                radio.setPersistent(false);
+                radio.setTitle(NO_SHORTCUT);
+                radio.setIcon(android.R.drawable.ic_delete);
+                radio.setLayoutResource(R.layout.preference_reversed_widget);
+
+            } else {
+                try {
+                    ApplicationInfo app = pm.getApplicationInfo(appTitle, PackageManager.GET_META_DATA);
+                    radio.setKey(app.packageName);
+                    radio.setPersistent(false);
+                    radio.setTitle(app.loadLabel(pm));
+                    radio.setIcon(app.loadIcon(pm));
+                    radio.setLayoutResource(R.layout.preference_reversed_widget);
+                } catch (Exception e) {
+                }
+            }
+            screen.addPreference(radio);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        RadioPreference radio = (RadioPreference)preference;
+        radio.clearOtherRadioPreferences(getPreferenceScreen());
+        String packageName = radio.getKey();
+
+        ShortcutManager.setShortcutPreference(keycode, packageName);
+
+        return true;
+    }
+}
