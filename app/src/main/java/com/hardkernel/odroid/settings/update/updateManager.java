85e4922fd8f1929692bacb83abf8be92693f85d6
Chris KIM <codewalker@hardkernel.com>
2019-09-24 09:34:49 +0900

    ODROID-N2: Fix update path for 64bit android.
    Change-Id: I4fa3acbdbda21c43b3481456eb875f5c713fb87f
    Signed-off-by: Chris KIM <codewalker@hardkernel.com>

--- app/src/main/java/com/hardkernel/odroid/settings/update/updateManager.java
+++ app/src/main/java/com/hardkernel/odroid/settings/update/updateManager.java
@@ -9,7 +9,7 @@ public class updateManager {
             "https://www.odroid.in/mirror/dn.odroid.com/S922X/ODROID-N2/Android/";
 
     public static final long PACKAGE_MAXSIZE = 500 * 1024 * 1024;   /* 500MB */
-    public static final String LATEST_VERSION = "latestupdate_pie";
+    public static final String LATEST_VERSION = "latestupdate_pie_64";
 
     public static final String KEY_OFFICIAL = "server_official";
     public static final String KEY_MIRROR = "server_mirror";
