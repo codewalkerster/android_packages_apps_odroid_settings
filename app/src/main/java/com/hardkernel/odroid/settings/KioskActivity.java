5c6127572f14b3f404789d2487be0ca9cf7d4658
Luke Go <sangch.go@gmail.com>
2020-02-27 18:21:16 +0900

    ODROID-Settings: Add kiosk mode options.
    - Kiosk mode is disable status bar and navigation bar.
    
    Change-Id: Ie138052a152768fe7a9571ba10848bbb8a62579c
    Signed-off-by: Luke Go <sangch.go@gmail.com>

---
+++ app/src/main/java/com/hardkernel/odroid/settings/KioskActivity.java
@@ -0,0 +1,22 @@
+package com.hardkernel.odroid.settings;
+
+import android.app.Fragment;
+
+public class KioskActivity extends TvSettingsActivity {
+    @Override
+    protected Fragment createSettingsFragment() {
+        return SettingsFragment.newInstance();
+    }
+
+    public static class SettingsFragment extends BaseSettingsFragment {
+        public static SettingsFragment newInstance() {
+            return new SettingsFragment();
+        }
+
+        @Override
+        public void onPreferenceStartInitialScreen() {
+            final KioskFragment fragment = KioskFragment.newInstance();
+            startPreferenceFragment(fragment);
+        }
+    }
+}
---
+++ app/src/main/java/com/hardkernel/odroid/settings/KioskFragment.java
@@ -0,0 +1,52 @@
+package com.hardkernel.odroid.settings;
+
+import android.os.Bundle;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.TwoStatePreference;
+import android.widget.Toast;
+
+public class KioskFragment extends LeanbackAddBackPreferenceFragment {
+
+    private static final String KEY_KIOSK_SWITCH = "kiosk_switch";
+    private static final String KIOSK_MODE = "kiosk_mode";
+    private TwoStatePreference kioskPref;
+
+    public static boolean kioskMode = false;
+
+    public static KioskFragment newInstance() {
+        return new KioskFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle saveInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.kisok, null);
+        kioskPref = (TwoStatePreference)findPreference(KEY_KIOSK_SWITCH);
+
+        kioskMode = (EnvProperty.getInt(KIOSK_MODE, 0) == 1);
+        kioskPref.setChecked(kioskMode);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        final String key = preference.getKey();
+        if (key == null)
+            return super.onPreferenceTreeClick(preference);
+
+        switch (key) {
+            case KEY_KIOSK_SWITCH:
+                kioskMode = kioskPref.isChecked();
+                EnvProperty.setAndSave(KIOSK_MODE, kioskMode? "1":"0", "Kiosk Mode");
+                Toast.makeText(getContext(),
+                        "Kiosk Mode will " + (kioskMode ? "": "not ")
+                                +"applied after reboot!",
+                        Toast.LENGTH_LONG).show();
+                return true;
+        }
+        return super.onPreferenceTreeClick(preference);
+    }
+}
