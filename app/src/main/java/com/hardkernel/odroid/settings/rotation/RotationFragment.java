84f3ff784ce2b6bb2d81e1dfa6027cb8e37c9d95
Luke Go <sangch.go@gmail.com>
2020-08-24 14:42:06 +0900

    ODROID-Settings: rotation: Add Landscape 180 degree.
    Change-Id: Ib2eeeeb289b9ec99a556546f0213934b09683e54

--- app/src/main/java/com/hardkernel/odroid/settings/rotation/RotationFragment.java
+++ app/src/main/java/com/hardkernel/odroid/settings/rotation/RotationFragment.java
@@ -27,7 +27,7 @@ public class RotationFragment extends LeanbackAddBackPreferenceFragment {
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         Display display = getActivity().getWindowManager().getDefaultDisplay();
         degree = display.getRotation();
-        if (degree != 0)
+        if (degree % 2 != 0)
             isPortrait = true;
         updatePreferenceFragment();
     }
@@ -51,6 +51,10 @@ public class RotationFragment extends LeanbackAddBackPreferenceFragment {
                     isPortrait = true;
                     degree = 1;
                     break;
+                case 180:
+                    isPortrait = false;
+                    degree = 2;
+                    break;
                 case 270:
                     isPortrait = true;
                     degree = 3;
@@ -61,7 +65,10 @@ public class RotationFragment extends LeanbackAddBackPreferenceFragment {
             android.provider.Settings.System.putInt(context.getContentResolver(), Settings.System.ACCELEROMETER_ROTATION, 0);
             android.provider.Settings.System.putInt(context.getContentResolver(), Settings.System.USER_ROTATION, degree);
         } else {
-            isPortrait = true;
+            if (preference.getKey().equals("landscape"))
+                isPortrait = false;
+            else
+                isPortrait = true;
         }
 
         updatePreferenceFragment();
@@ -76,15 +83,29 @@ public class RotationFragment extends LeanbackAddBackPreferenceFragment {
         setPreferenceScreen(screen);
 
         RadioPreference landscape;
+        RadioPreference landscapeReverse;
+        Preference land;
         Preference portrait;
-        RadioPreference portrait_90 = null;
-        RadioPreference portrait_270 = null;
+        RadioPreference portrait_90;
+        RadioPreference portrait_270;
+
+        land = new Preference(themedContext);
+        land.setKey("landscape");
+        land.setPersistent(false);
+        land.setTitle(R.string.landscape);
+        land.setLayoutResource(R.layout.preference_reversed_widget);
 
         landscape = new RadioPreference(themedContext);
         landscape.setKey("0");
         landscape.setPersistent(false);
-        landscape.setTitle(R.string.landscape);
+        landscape.setTitle(R.string.landscape_0);
         landscape.setLayoutResource(R.layout.preference_reversed_widget);
+
+        landscapeReverse = new RadioPreference(themedContext);
+        landscapeReverse.setKey("180");
+        landscapeReverse.setPersistent(false);
+        landscapeReverse.setTitle(R.string.landscape_180);
+        landscapeReverse.setLayoutResource(R.layout.preference_reversed_widget);
 
         portrait = new Preference(themedContext);
         portrait.setKey("portrait");
@@ -92,19 +113,18 @@ public class RotationFragment extends LeanbackAddBackPreferenceFragment {
         portrait.setTitle(R.string.portrait);
         portrait.setLayoutResource(R.layout.preference_reversed_widget);
 
-        if (isPortrait) {
-            portrait_90 = new RadioPreference(themedContext);
-            portrait_90.setKey("90");
-            portrait_90.setPersistent(false);
-            portrait_90.setTitle(R.string.portrait_90);
-            portrait_90.setLayoutResource(R.layout.preference_reversed_widget);
+        portrait_90 = new RadioPreference(themedContext);
+        portrait_90.setKey("90");
+        portrait_90.setPersistent(false);
+        portrait_90.setTitle(R.string.portrait_90);
+        portrait_90.setLayoutResource(R.layout.preference_reversed_widget);
 
-            portrait_270 = new RadioPreference(themedContext);
-            portrait_270.setKey("270");
-            portrait_270.setPersistent(false);
-            portrait_270.setTitle(R.string.portrait_270);
-            portrait_270.setLayoutResource(R.layout.preference_reversed_widget);
-        }
+        portrait_270 = new RadioPreference(themedContext);
+        portrait_270.setKey("270");
+        portrait_270.setPersistent(false);
+        portrait_270.setTitle(R.string.portrait_270);
+        portrait_270.setLayoutResource(R.layout.preference_reversed_widget);
+
         switch (degree) {
             case 0:
                 landscape.setChecked(true);
@@ -112,12 +132,19 @@ public class RotationFragment extends LeanbackAddBackPreferenceFragment {
             case 1:
                 portrait_90.setChecked(true);
                 break;
+            case 2:
+                landscapeReverse.setChecked(true);
+                break;
             case 3:
                 portrait_270.setChecked(true);
                 break;
         }
 
-        screen.addPreference(landscape);
+        screen.addPreference(land);
+        if (!isPortrait) {
+            screen.addPreference(landscape);
+            screen.addPreference(landscapeReverse);
+        }
         screen.addPreference(portrait);
         if (isPortrait) {
             screen.addPreference(portrait_90);
