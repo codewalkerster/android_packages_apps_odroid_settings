420079cc70e74c93c59418920083d29ade1bbb26
Luke go <sangch.go@gmail.com>
2021-09-15 17:32:58 +0900

    ODROID-Settings: Ported CPU menus.
    - TODO: Add a CPU menu icon, Add configuration saving methods.
    
    Change-Id: I4588257028601b70f96030e26822d65495b18269
    Signed-off-by: xiane <sangch.go@gmail.com>

---
+++ Settings/src/hardkernel/odroid/settings/cpu/governor/GovernorFragment.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+package hardkernel.odroid.settings.cpu.governor;
+
+import android.content.Context;
+import android.os.Bundle;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import hardkernel.odroid.settings.R;
+import hardkernel.odroid.settings.SettingsPreferenceFragment;
+
+import hardkernel.odroid.settings.RadioPreference;
+//import hardkernel.odroid.settings.ConfigEnv;
+import hardkernel.odroid.settings.cpu.CPU;
+
+public class GovernorFragment extends SettingsPreferenceFragment {
+    private static final String TAG = "GovernorFragment";
+    public static CPU cpu = null;
+
+    public static GovernorFragment newInstance() { return new GovernorFragment(); }
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        if (cpu == null)
+            cpu = CPU.getCPU(TAG, CPU.Cluster.Big);
+        updatePreferenceFragment();
+    }
+
+    private void updatePreferenceFragment() {
+        final Context themedContext = getPreferenceManager().getContext();
+        final PreferenceScreen screen = getPreferenceManager().createPreferenceScreen(themedContext);
+
+        if (cpu.cluster == CPU.Cluster.Big)
+            screen.setTitle(R.string.big_core_governor);
+        else if (cpu.cluster == CPU.Cluster.Little)
+            screen.setTitle(R.string.little_core_governor);
+        else
+            screen.setTitle(R.string.cpu);
+
+        setPreferenceScreen(screen);
+
+        String[] governorList = cpu.governor.getGovernors();
+
+        for (final String governor : governorList) {
+            final RadioPreference radioPreference = new RadioPreference(themedContext);
+            radioPreference.setKey(governor);
+            radioPreference.setPersistent(false);
+            radioPreference.setTitle(governor);
+            radioPreference.setLayoutResource(R.layout.preference_reversed_widget);
+            if (cpu.governor.getCurrent().equals(governor)) {
+                radioPreference.setChecked(true);
+            }
+            screen.addPreference(radioPreference);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        if (preference instanceof RadioPreference) {
+            final RadioPreference radioPreference = (RadioPreference)preference;
+            radioPreference.clearOtherRadioPreferences(getPreferenceScreen());
+            if (radioPreference.isChecked()) {
+                String selectedGovernor = radioPreference.getKey();
+                cpu.governor.set(selectedGovernor);
+                saveGovernor(selectedGovernor);
+                radioPreference.setChecked(true);
+            } else {
+                radioPreference.setChecked(true);
+            }
+        }
+        return super.onPreferenceTreeClick(preference);
+    }
+
+    private void saveGovernor(String governor) {
+        /*
+        if (cpu.cluster == CPU.Cluster.Little)
+            ConfigEnv.setLittleCoreGovernor(governor);
+        else if (cpu.cluster == CPU.Cluster.Big)
+            ConfigEnv.setBigCoreGovernor(governor);
+            */
+    }
+}
