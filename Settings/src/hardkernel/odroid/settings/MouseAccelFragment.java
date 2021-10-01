9f1cd6825589eab29110d67db7666abbbfbadbc4
Luke go <sangch.go@gmail.com>
2021-10-01 11:41:43 +0900

    ODROIDSettings: Mouse Acceleration.
    Change-Id: I8ea7b8fa6f2beeb5a5ec67d67e266358c61f6f37

---
+++ Settings/src/hardkernel/odroid/settings/MouseAccelFragment.java
@@ -0,0 +1,53 @@
+package hardkernel.odroid.settings;
+
+import android.os.Bundle;
+import androidx.preference.Preference;
+import androidx.preference.TwoStatePreference;
+import android.widget.Toast;
+
+public class MouseAccelFragment extends LeanbackAddBackPreferenceFragment {
+
+    private static final String KEY_MOUSE_ACCELERATION_SWITCH = "mouse_accel_switch";
+    private static final String MOUSE_ACCELERATION = "persist.mouse_acceleration";
+    private TwoStatePreference mouseAccelPref;
+
+    public static boolean mouseAccel = true;
+
+    public static MouseAccelFragment newInstance() {
+        return new MouseAccelFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle saveInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.mouse_accel, null);
+        mouseAccelPref = (TwoStatePreference) findPreference(KEY_MOUSE_ACCELERATION_SWITCH);
+
+        mouseAccel = EnvProperty.getBoolean(MOUSE_ACCELERATION, true);
+        mouseAccelPref.setChecked(mouseAccel);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        final String key = preference.getKey();
+        if (key == null)
+            return super.onPreferenceTreeClick(preference);
+
+        switch (key) {
+            case KEY_MOUSE_ACCELERATION_SWITCH:
+                mouseAccel = mouseAccelPref.isChecked();
+                EnvProperty.set(MOUSE_ACCELERATION, String.valueOf(mouseAccel));
+                Toast.makeText(getContext(),
+                        "Mouse Acceleration will be "
+                        + (mouseAccel ? "enabled":"disabled")
+                        + " after reboot!",
+                        Toast.LENGTH_LONG).show();
+                return true;
+        }
+        return super.onPreferenceTreeClick(preference);
+    }
+}
