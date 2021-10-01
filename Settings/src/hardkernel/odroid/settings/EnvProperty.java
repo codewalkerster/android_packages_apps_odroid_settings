f2a80bf4156777fd5eaf472c1b4b2034d1263f1d
Luke go <sangch.go@gmail.com>
2021-10-01 11:41:43 +0900

    ODROIDSettings: Mouse Acceleration.
    Change-Id: I8ea7b8fa6f2beeb5a5ec67d67e266358c61f6f37

---
+++ Settings/src/hardkernel/odroid/settings/EnvProperty.java
@@ -0,0 +1,27 @@
+package hardkernel.odroid.settings;
+
+import android.os.SystemProperties;
+
+public class EnvProperty {
+    public static int getInt(String key, int defaultValue) {
+        return SystemProperties.getInt(key, defaultValue);
+    }
+
+    public static boolean getBoolean(String key, boolean defaultValue) {
+        return SystemProperties.getBoolean(key, defaultValue);
+    }
+
+    public static String get (String key) {
+        return SystemProperties.get(key);
+    }
+
+    public static String get (String key, String defaultValue) {
+        return SystemProperties.get(key, defaultValue);
+    }
+
+    public static boolean set (String key, String value) {
+        SystemProperties.set(key, value);
+
+        return true;
+    }
+}
