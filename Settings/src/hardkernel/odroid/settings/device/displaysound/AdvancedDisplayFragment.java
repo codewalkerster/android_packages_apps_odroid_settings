31cceb113d3d51b46fee71503dcc3a7ab1e646f1
Luke go <sangch.go@gmail.com>
2021-10-15 12:41:19 +0900

    ODROIDSettings: Add Kiosk mode switch.
    - The switch is added to display & sound -> Advanced display settings.
    
    Change-Id: I1808c694cab1026b0358de47520a198a20f08459
    Signed-off-by: Luke go <sangch.go@gmail.com>

--- Settings/src/hardkernel/odroid/settings/device/displaysound/AdvancedDisplayFragment.java
+++ Settings/src/hardkernel/odroid/settings/device/displaysound/AdvancedDisplayFragment.java
@@ -21,6 +21,7 @@ import static android.provider.Settings.Secure.MINIMAL_POST_PROCESSING_ALLOWED;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.text.TextUtils;
+import android.widget.Toast;
 
 import androidx.annotation.Keep;
 import androidx.preference.Preference;
@@ -29,6 +30,7 @@ import androidx.preference.SwitchPreference;
 import com.android.internal.logging.nano.MetricsProto;
 import hardkernel.odroid.settings.R;
 import hardkernel.odroid.settings.SettingsPreferenceFragment;
+import hardkernel.odroid.settings.EnvProperty;
 
 /**
  * The "Advanced display settings" screen in TV Settings.
@@ -36,6 +38,10 @@ import hardkernel.odroid.settings.SettingsPreferenceFragment;
 @Keep
 public class AdvancedDisplayFragment extends SettingsPreferenceFragment {
     private static final String KEY_GAME_MODE = "game_mode";
+    private static final String KEY_KIOSK_MODE = "kiosk_mode";
+
+    private static final String PERSIST_KIOSK_MODE = "persist.kiosk_mode";
+    private static Boolean kiosk_mode = false;
 
     @Override
     public int getMetricsCategory() {
@@ -47,12 +53,22 @@ public class AdvancedDisplayFragment extends SettingsPreferenceFragment {
         setPreferencesFromResource(R.xml.advanced_display, null);
         SwitchPreference gameModePreference = findPreference(KEY_GAME_MODE);
         gameModePreference.setChecked(getGameModeStatus() == 1);
+
+        kiosk_mode = EnvProperty.getBoolean(PERSIST_KIOSK_MODE, false);
+        SwitchPreference kioskModePreference = findPreference(KEY_KIOSK_MODE);
+        kioskModePreference.setChecked(kiosk_mode);
     }
 
     @Override
     public boolean onPreferenceTreeClick(Preference preference) {
         if (TextUtils.equals(preference.getKey(), KEY_GAME_MODE)) {
             setGameModeStatus(((SwitchPreference) preference).isChecked() ? 1 : 0);
+        } else if (TextUtils.equals(preference.getKey(), KEY_KIOSK_MODE)) {
+            kiosk_mode = ((SwitchPreference) preference).isChecked();
+            EnvProperty.set(PERSIST_KIOSK_MODE, kiosk_mode);
+            Toast.makeText(getContext(),
+                    R.string.kiosk_mode_message,
+                    Toast.LENGTH_LONG).show();
         }
         return super.onPreferenceTreeClick(preference);
     }
