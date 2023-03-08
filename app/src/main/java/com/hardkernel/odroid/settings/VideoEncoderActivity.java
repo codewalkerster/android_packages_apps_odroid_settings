f5d4ebfa310f36579f68cef1195b7c1785b6d2e4
Luke Go <sangch.go@gmail.com>
2023-03-08 11:35:48 +0900

    ODROID-Settings: Add Video Encoder menu.
    Signed-off-by: Luke Go <sangch.go@gmail.com>
    Change-Id: If83ba0456660f74a29313d2efa2b914f7e5dba7c

---
+++ app/src/main/java/com/hardkernel/odroid/settings/VideoEncoderActivity.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hardkernel.odroid.settings;
+
+import android.app.Fragment;
+
+import com.hardkernel.odroid.settings.BaseSettingsFragment;
+import com.hardkernel.odroid.settings.TvSettingsActivity;
+
+/**
+ * Activity that Change the Video Encoder.
+ */
+public class VideoEncoderActivity extends TvSettingsActivity {
+
+    @Override
+    protected Fragment createSettingsFragment() {
+        return SettingsFragment.newInstance();
+    }
+
+    public static class SettingsFragment extends BaseSettingsFragment {
+
+        public static SettingsFragment newInstance() {
+            return new SettingsFragment();
+        }
+
+        @Override
+        public void onPreferenceStartInitialScreen() {
+            final VideoEncoderFragment fragment = VideoEncoderFragment.newInstance();
+            startPreferenceFragment(fragment);
+        }
+    }
+}
---
+++ app/src/main/java/com/hardkernel/odroid/settings/VideoEncoderFragment.java
@@ -0,0 +1,111 @@
+package com.hardkernel.odroid.settings;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v17.preference.LeanbackPreferenceFragment;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceScreen;
+import android.widget.Toast;
+import com.hardkernel.odroid.settings.EnvProperty;
+import com.hardkernel.odroid.settings.R;
+import com.hardkernel.odroid.settings.RadioPreference;
+import com.hardkernel.odroid.settings.util.DroidUtils;
+
+public class VideoEncoderFragment extends LeanbackAddBackPreferenceFragment {
+    final String TAG= "VideoEncoderFragment";
+
+    private String[] encoderNames;
+    private String[] encoderValues;
+    private static String currentEncoder;
+    private static final String ENCODER_RADIO_GROUP = "encoder_group";
+    private PreferenceCategory categoryPref;
+    private static long totalSize = 0;
+    private static final long MEM_2G = 2147483648L;
+
+    public static VideoEncoderFragment newInstance() {
+        return new VideoEncoderFragment();
+    }
+
+    private String[] getArrayString(int resid) {
+        return getActivity().getResources().getStringArray(resid);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        encoderNames = getArrayString(R.array.media_encoder_names);
+        encoderValues = getArrayString(R.array.media_encoder_values);
+        currentEncoder = EnvProperty.getFromFile("media.settings.xml");
+
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstance, String rootKey) {
+        final Context themedContext = getPreferenceManager().getContext();
+        final PreferenceScreen screen = getPreferenceManager().createPreferenceScreen(themedContext);
+        screen.setTitle(R.string.encoder_title);
+        categoryPref = new PreferenceCategory(themedContext);
+        categoryPref.setTitle(R.string.default_encoder);
+        screen.addPreference(categoryPref);
+
+        for (int i=0; i < encoderNames.length; i++) {
+            if (DroidUtils.isOdroidN2()
+                    && encoderNames[i].contains("H265")
+                    && (getTotalMemSize() < MEM_2G))
+                continue;
+            final String value = encoderValues[i];
+            final RadioPreference radio = new RadioPreference(themedContext);
+            radio.setKey(value);
+            radio.setPersistent(false);
+            radio.setTitle(encoderNames[i]);
+            radio.setRadioGroup(ENCODER_RADIO_GROUP);
+            radio.setLayoutResource(R.layout.preference_reversed_widget);
+
+            if (value.equals(currentEncoder)) {
+                radio.setChecked(true);
+            }
+
+            categoryPref.addPreference(radio);
+        }
+        setPreferenceScreen(screen);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        if (preference instanceof RadioPreference) {
+            final RadioPreference radio = (RadioPreference)preference;
+            radio.clearOtherRadioPreferences(categoryPref);
+            if (radio.isChecked()) {
+                String key = radio.getKey().toString();
+                if (!key.equals(currentEncoder)) {
+                    Toast.makeText(getContext(),
+                            "Changed Video Encoder to "
+                            + radio.getTitle()
+                            + ", and it will be applied after reboot!",
+                            Toast.LENGTH_LONG).show();
+                    currentEncoder = key;
+                    EnvProperty.save("media.settings.xml", key,
+                            "Change video encoder to " + radio.getTitle());
+                }
+            } else {
+                radio.setChecked(true);
+            }
+        }
+        return super.onPreferenceTreeClick(preference);
+    }
+
+    private long getTotalMemSize() {
+        if (totalSize != 0)
+            return totalSize;
+
+        ActivityManager am = (ActivityManager)getContext()
+            .getSystemService(Context.ACTIVITY_SERVICE);
+        ActivityManager.MemoryInfo memInfo = new ActivityManager.MemoryInfo();
+        am.getMemoryInfo(memInfo);
+        totalSize = memInfo.totalMem;
+
+        return totalSize;
+    }
+}
