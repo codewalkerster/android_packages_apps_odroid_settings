7d97d1a70129b82cdc3e3ed6a66642f2468b6bfb
xiane <sangch.go@gmail.com>
2025-01-14 14:28:52 +0900

    ODROID-Settings: Set USER_SETUP_COMPLETE configs.
    Signed-off-by: xiane <sangch.go@gmail.com>
    Change-Id: Ibc314cb16978e65adb54b222bd43b14f020824d6

--- Settings/src/hardkernel/odroid/settings/OdroidSettingsReceiver.java
+++ Settings/src/hardkernel/odroid/settings/OdroidSettingsReceiver.java
@@ -3,6 +3,7 @@ package hardkernel.odroid.settings;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.provider.Settings;
 
 import hardkernel.odroid.settings.cpu.CpuReceiver;
 import hardkernel.odroid.settings.gpu.GpuReceiver;
@@ -12,10 +13,19 @@ public class OdroidSettingsReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        skipUserSetup(context);
         if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
             CpuReceiver.onReceive();
             GpuReceiver.onReceive();
             ShortcutManager.onReceive(context);
+        }
+    }
+
+    private void skipUserSetup(Context context) {
+        if (Settings.Secure.getInt(context.getContentResolver(), Settings.Secure.TV_USER_SETUP_COMPLETE, 0) == 0) {
+            Settings.Global.putInt(context.getContentResolver(), Settings.Global.DEVICE_PROVISIONED, 1);
+            Settings.Secure.putInt(context.getContentResolver(), Settings.Secure.USER_SETUP_COMPLETE, 1);
+            Settings.Secure.putInt(context.getContentResolver(), Settings.Secure.TV_USER_SETUP_COMPLETE, 1);
         }
     }
 }
